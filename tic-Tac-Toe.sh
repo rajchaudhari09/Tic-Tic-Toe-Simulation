f790caaf0df843de689d42912156b41660acd757
rajchaudhari09 <rajchaudhari366@gmail.com>
2020-03-25 19:14:57 +0530

    ADDED: If the computer chance

--- tic-Tac-Toe.sh
+++ tic-Tac-Toe.sh
@@ -56,13 +56,13 @@ function assignSymbol()
 #Function For Whoes Play First 
 function toss() 
 {
-state=$((RANDOM%2))
-   if [[ $state -eq 1 ]]
-   then
-      echo "First Is The Player"
-   else
-      echo "First Is The Computer"
-   fi
+	state=$((RANDOM%2))
+   	if [[ $state -eq 1 ]]
+   	then
+      		echo "Player"
+   	else
+      		echo "Computer"
+   	fi
 }
 
 #Function For Which Position Symbol Are Placed
@@ -71,7 +71,7 @@ function ticTacToe()
 	local win=0
 		#Use The Condition Symbol ||X|| Printed That
 		if [[ ${Board[0,0]} == "X" && ${Board[0,1]} == "X" && ${Board[0,2]} == "X" ]]
-	   then
+	   	then
 			win=1
 		elif [[ ${Board[1,0]} == "X" && ${Board[1,1]} == "X" && ${Board[1,2]} == "X" ]] 
 		then
@@ -95,46 +95,71 @@ function ticTacToe()
 		then
 			win=1
 		fi
+
 		#Use The Condition Symbol ||O|| Printed That
 		if [[ ${Board[0,0]} == "O" && ${Board[0,1]} == "O" && ${Board[0,2]} == "O" ]] 
 		then
-      	win=1
-    	elif [[ ${Board[1,0]} == "O" && ${Board[1,1]} == "O" && ${Board[1,2]} == "O" ]] 
+      			win=1
+    		elif [[ ${Board[1,0]} == "O" && ${Board[1,1]} == "O" && ${Board[1,2]} == "O" ]] 
 		then
-      	win=1
-    	elif [[ ${Board[2,0]} == "O" && ${Board[2,1]} == "O" && ${Board[2,2]} == "O" ]] 
+      			win=1
+    		elif [[ ${Board[2,0]} == "O" && ${Board[2,1]} == "O" && ${Board[2,2]} == "O" ]] 
 		then
-      	win=1
-    	elif [[ ${Board[0,0]} == "O" && ${Board[1,0]} == "O" && ${Board[2,0]} == "O" ]] 
+      			win=1
+    		elif [[ ${Board[0,0]} == "O" && ${Board[1,0]} == "O" && ${Board[2,0]} == "O" ]] 
 		then
-      	win=1
-    	elif [[ ${Board[0,1]} == "O" && ${Board[1,1]} == "O" && ${Board[2,1]} == "O" ]] 
+      			win=1
+    		elif [[ ${Board[0,1]} == "O" && ${Board[1,1]} == "O" && ${Board[2,1]} == "O" ]] 
 		then
-      	win=1
-   	elif [[ ${Board[0,2]} == "O" && ${Board[1,2]} == "O" && ${Board[2,2]} == "O" ]] 
+      			win=1
+   		elif [[ ${Board[0,2]} == "O" && ${Board[1,2]} == "O" && ${Board[2,2]} == "O" ]] 
 		then
-      	win=1
-    	elif [[ ${Board[0,0]} == "O" && ${Board[1,1]} == "O" && ${Board[2,2]} == "O" ]] 
+      			win=1
+    		elif [[ ${Board[0,0]} == "O" && ${Board[1,1]} == "O" && ${Board[2,2]} == "O" ]] 
 		then
-      	win=1
-    	elif [[ ${Board[0,2]} == "O" && ${Board[1,1]} == "O" && ${Board[2,0]} == "O" ]] 
+      			win=1
+    		elif [[ ${Board[0,2]} == "O" && ${Board[1,1]} == "O" && ${Board[2,0]} == "O" ]] 
 		then
-      	win=1
-   	fi
-	#Use The Condition For Player Is Won Or Tie
-	if [[ $won == 1 ]]
-	then
-		printf "Congratulation....!!!! $player Is Won The Match"
-		printf "huuuurrrrrryyyyyyyyyyy!!!!!@@@@@@!!!!!! "
-		displayBoard
+      			win=1
+   		fi
+
+		#Use The Condition For Player Is Won Or Tie
+		if [[ $win == 1 ]]
+		then
+			printf "Congratulation....!!!! $playerSymbol Is Won The Match.......huuuurrrrrryyyyyyyyyyy!!!!!@@@@@@!!!!!!"
+		printBoard
 	   	exit
-	fi
-	if [[ $n == 8 ]]
-	then
-		displayBoard
-		printf "This match is a Tie"
+		fi
+
+		if [[ $n == 8 ]]
+		then
+			printBoard
+			printf "This match is a Tie"
 		exit
+		fi
+}
+
+#Function For The Putting Position To Computer Side
+function computerPlaying() 
+{
+	computerPosition=$((RANDOM%9))
+	newRowIndex=$(($computerPosition / 3))
+	newColumnIndex=$(($computerPosition % 3))
+	if [[ ${Board[$newRowIndex,$newColumnIndex]} == "X" || ${Board[$newRowIndex,$newColumnIndex]} == "O" ]]
+	 then
+		computerPlaying
+	 else
+		Board[$newRowIndex,$newColumnIndex]=$playerSymbol
 	fi
+}
+
+#Function For The Putting Position To User Side
+function userPlaying() 
+{
+	read -p "Putting Your Position: " userPosition
+	newRowIndex=$(($userPosition / 3))
+	newColumnIndex=$(($userPosition % 3))
+	Board[$newRowIndex,$newColumnIndex]=$playerSymbol
 }
 
 #Store toss Function In A New Variable
@@ -147,24 +172,40 @@ boardCreating
 #Function For The Position Assigning The Board
 function playGame()
 {
+	if [[ $chance -eq "Player" ]]
+	then
+		p=0
+		printf "Player will play first"
+	else
+		p=1
+		printf "Computer will play first"
+	fi
+
+	#Call Function Print Board
+	printBoard
+
+
 	while [[ $n -lt 9 ]]
 	do
-		#Store AssignSymbol Function In A New Variable
-		player=$( assignSymbol )
 
-		#Call Function Print Board
+		if [[ $(($p % 2)) -eq 0 ]]
+		then
+			#Store AssignSymbol Function In A New Variable
+			playerSymbol=$( assignSymbol )
+			#Call Function The User Play To The Game 
+			userPlaying
+		else
+			playerSymbol=$( assignSymbol )
+			#Call Function The System Play To The Game 
+			computerPlaying
+		fi
+
 		printBoard
-
-		read -p "Putting Your Position: " position
-
-		#Check The Position Row And Column For A Player
-		newRowIndex=$(($position / 3))
-		newColumnIndex=$(($position % 3))
-		Board[$newRowIndex,$newColumnIndex]=$player
 
 		#Call Function TicTacToe 
 		ticTacToe
 		((n++))
+	((p++))
 	done
 }
 playGame
